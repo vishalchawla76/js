b84d7381a7eae440322cf70c6b723de08fb7a4ef
vishalchawla76 <82635791+vishalchawla76@users.noreply.github.com>
2024-09-26 13:15:07 +0000

    datatype-summary added

---
+++ js_basics/comparision.js
@@ -0,0 +1,7 @@
+console.log("1" + 2);
+console.log(1 + "2");
+console.log(1 == "2");
+// "===" compareing along with the datatype and "==" do conversion of datatype and compares
+console.log(1 === "1");
+
+
