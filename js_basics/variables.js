eac962a53edb22b4268f36ffd4cee8d0f233fc1a
vishalchawla76 <82635791+vishalchawla76@users.noreply.github.com>
2024-09-26 07:45:36 +0000

    variables and datatypes

---
+++ js_basics/variables.js
@@ -0,0 +1,16 @@
+const AccountId = 144553
+let AccEmail = "visc@gmail.com"
+var AccPassword = "12345"
+AccCity = "mumbai"
+AccEmail = "ab@ab.com"
+AccCity = "jaipur"
+AccPassword = "28432"
+
+//AccountId = 5// not allowed
+console.log(AccountId);
+console.table([AccEmail,AccPassword,AccountId,AccCity])
+/*
+prefer not to use var
+
+*/
+
