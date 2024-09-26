b0e1a41dbe7ae03a203f647bf9bea48d2c083f56
vishalchawla76 <82635791+vishalchawla76@users.noreply.github.com>
2024-09-26 10:43:14 +0000

    conversionoperation

---
+++ js_basics/conversionoperation.js
@@ -0,0 +1,24 @@
+let score = "33"
+console.log(typeof score)
+console.log(typeof(score))
+let newvar = Number(score)
+console.log(typeof newvar)
+console.log(newvar)
+console.log("-------------\n\n\n")
+
+
+let score2 = "33ab"
+let newvar2 = Number(score2)
+console.log(typeof score2)
+console.log(typeof newvar2)
+console.log(newvar2)
+console.log("-----------\n\n\n")
+
+//*********************operations********************* */
+let val = 3
+let negval = -val
+console.log("negval = " + negval)
+
+
+
+
---
+++ js_basics/datatypes.js
@@ -0,0 +1,13 @@
+/*
+number
+bigint
+string
+boolean
+null
+undefined
+symbol
+object
+NaN
+*/
+
+console.table([typeof null, typeof "vishal", typeof 2, typeof true])
